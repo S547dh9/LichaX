4ad3972ac4781edde5341019ef420acf2ecf0e18
Maya <168336946+Mayaemx@users.noreply.github.com>
2024-07-04 22:35:21 +0530

    Update speed_text.py

--- AvishaRobot/modules/speed_text.py
+++ AvishaRobot/modules/speed_text.py
@@ -34,7 +34,7 @@ def speedtestxyz_callback(update: Update, context: CallbackContext):
         speed.get_best_server()
         speed.download()
         speed.upload()
-        replymsg = "⬤ sᴩᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛ ʙʏ ➥ ˹ ᴀᴠɪsʜᴀ ꭙ ʀᴏʙᴏᴛ™ ♡゙"
+        replymsg = "⬤ sᴩᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛ ʙʏ ➥ ᴍs ʟɪcʜᴀ"
 
         if query.data == "speedtest_image":
             speedtest_image = speed.results.share()
@@ -45,10 +45,10 @@ def speedtestxyz_callback(update: Update, context: CallbackContext):
 
         elif query.data == "speedtest_text":
             result = speed.results.dict()
-            replymsg += f"\n\n⬤ ᴅᴏᴡɴʟᴏᴀᴅ ➥ `{convert(result['download'])} ᴍʙ/s`\n● ᴜᴘʟᴏᴀᴅ ➥ `{convert(result['upload'])}ᴍʙ/s`\n● ᴘɪɴɢ ➥ `{result['ping']}`\n\n❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ˹ ᴀᴠɪsʜᴀ ꭙ ʀᴏʙᴏᴛ™ ♡゙"
+            replymsg += f"\n\n⬤ ᴅᴏᴡɴʟᴏᴀᴅ ➥ `{convert(result['download'])} ᴍʙ/s`\n● ᴜᴘʟᴏᴀᴅ ➥ `{convert(result['upload'])}ᴍʙ/s`\n● ᴘɪɴɢ ➥ `{result['ping']}`\n\n❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ᴍs ʟɪcʜᴀ"
             update.effective_message.edit_text(replymsg, parse_mode=ParseMode.MARKDOWN)
     else:
-        query.answer("⬤ ʏᴏᴜ ᴀʀᴇ ʀᴇǫᴜɪʀᴇᴅ ᴛᴏ ᴊᴏɪɴ @the_friendz ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ.")
+        query.answer("⬤ ʏᴏᴜ ᴀʀᴇ ʀᴇǫᴜɪʀᴇᴅ ᴛᴏ ᴊᴏɪɴ @phoenixxsupport ᴛᴏ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ.")
 
 
 SPEED_TEST_HANDLER = DisableAbleCommandHandler(
