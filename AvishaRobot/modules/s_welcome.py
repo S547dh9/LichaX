3564176a57c77e5fd389b1f475e31d0fa26cf04b
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-07-06 20:59:12 +0530

    Update s_welcome.py

--- AvishaRobot/modules/s_welcome.py
+++ AvishaRobot/modules/s_welcome.py
@@ -34,7 +34,7 @@ def circle(pfp, size=(450, 450)):
     return pfp
 
 def welcomepic(pic, user, chat, id, uname):
-    background = Image.open("AvishaRobot/resources/bgg.png")
+    background = Image.open("AvishaRobot/resources/img.jpg")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp)
     pfp = pfp.resize(
