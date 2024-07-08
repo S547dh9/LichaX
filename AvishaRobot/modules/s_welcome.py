4e1b7527c6331f5efd1295a0e0b13ca6e5883b2c
Maya <168336946+Mayaemx@users.noreply.github.com>
2024-07-08 19:05:35 +0530

    Update s_welcome.py

--- AvishaRobot/modules/s_welcome.py
+++ AvishaRobot/modules/s_welcome.py
@@ -22,6 +22,37 @@ class temp:
     U_NAME = None
     B_NAME = None
 
+def circle(pfp, size=(450, 450)):
+    pfp = pfp.resize(size, Image.ANTIALIAS).convert("RGBA")
+    bigsize = (pfp.size[0] * 3, pfp.size[1] * 3)
+    mask = Image.new("L", bigsize, 0)
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0) + bigsize, fill=255)
+    mask = mask.resize(pfp.size, Image.ANTIALIAS)
+    mask = ImageChops.darker(mask, pfp.split()[-1])
+    pfp.putalpha(mask)
+    return pfp
+
+def welcomepic(pic, user, chat, id, uname):
+    background = Image.open("AvishaRobot/resources/bg.jpg")
+    pfp = Image.open(pic).convert("RGBA")
+    pfp = circle(pfp)
+    pfp = pfp.resize(
+        (450, 450)
+    ) 
+    draw = ImageDraw.Draw(background)
+    font = ImageFont.truetype('AvishaRobot/resources/SwanseaBold-D0ox.ttf', size=44)
+    welcome_font = ImageFont.truetype('AvishaRobot/resources/SwanseaBold-D0ox.ttf', size=90)
+    draw.text((65, 250), f'NAME : {unidecode(user)}', fill=(255, 255, 255), font=font)
+    draw.text((65, 340), f'ID : {id}', fill=(255, 255, 255), font=font)
+    draw.text((65,430), f"USERNAME : {uname}", fill=(255,255,255),font=font)
+    pfp_position = (767, 133)  
+    background.paste(pfp, pfp_position, pfp)  
+    background.save(
+        f"downloads/welcome#{id}.png"
+    )
+    return f"downloads/welcome#{id}.png"
+
 CUTE = """**
 @app.on_message(filters.command("zwelcome", COMMAND_HANDLER) & ~filters.private)
 async def auto_state(_, message):
