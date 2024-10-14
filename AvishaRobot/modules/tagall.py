ab558ad8c9ec172b8742dc392a235effbf7fec85
S547dh9 <84910430+S547dh9@users.noreply.github.com>
2024-10-14 18:53:59 +0530

    Update tagall.py

--- AvishaRobot/modules/tagall.py
+++ AvishaRobot/modules/tagall.py
@@ -5,7 +5,7 @@ from telethon.errors import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 
-from AvishaRobot import telethn as client
+from ShikimoriSan import telethn as client
 
 spam_chats = []
 
